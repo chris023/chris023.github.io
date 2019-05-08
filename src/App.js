6398e65f4f67ee5570422bf73a64274a02a404af
chris023 <chris.meyer023@gmail.com>
2019-05-08 09:43:12 -0600

    Apply linter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { withRouter, Route, Switch } from 'react-router-dom'
+import Home from './paths/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Switch>
+    <Route path="/" component={Home} />
+  </Switch>
+)
 
-export default App;
+export default withRouter(App)
