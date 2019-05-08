140dde34a184f5e38dd8fb3e283b24c435010380
chris023 <chris.meyer023@gmail.com>
2019-05-08 09:47:40 -0600

    Add redux pkg and update imports

--- src/util/redux/store.js
+++ src/util/redux/store.js
@@ -1,5 +1,5 @@
-import { compose } from 'react-redux'
-import { createStore } from 'react-redux'
+import { compose } from 'redux'
+import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import rootReducer from './reducers'
