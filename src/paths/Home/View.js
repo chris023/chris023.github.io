dee8aebee78725e3858f4c5c9264bf1578c63d43
chris023 <chris.meyer023@gmail.com>
2019-05-08 12:21:37 -0600

    Add root styling

--- src/paths/Home/View.js
+++ src/paths/Home/View.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core'
+import { Landing } from './'
 
-const View = () => <p>hi</p>
+const styles = theme => ({
+  root: {
+    boxSizing: 'border-box',
+    minHeight: '100vh',
+    paddingTop: theme.mixins.toolbar.minHeight + 8,
+    paddingLeft: theme.spacing.unit * 7 + 1,
+  },
+})
 
-export default View
+const View = ({ classes }) => {
+  return (
+    <div className={classes.root}>
+      <Landing />
+    </div>
+  )
+}
+
+View.propTypes = {
+  classes: PropTypes.object,
+}
+
+export default withStyles(styles)(View)
