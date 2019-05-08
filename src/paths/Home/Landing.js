92d46fb20a82a5a91e1b2600d1401b6205984477
chris023 <chris.meyer023@gmail.com>
2019-05-08 12:54:36 -0600

    Add background video and center title

--- src/paths/Home/Landing.js
+++ src/paths/Home/Landing.js
@@ -2,20 +2,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Paper, Typography, withStyles } from '@material-ui/core'
+import bgvid from '../../assets/bgvid.webm'
 
 const styles = theme => ({
   root: {
+    background: 'none',
     boxSizing: 'border-box',
     height: 'calc(100vh - 64px)',
     padding: theme.spacing.unit * 4,
+  },
+  bgvid: {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    minWidth: '100%',
+    minHeight: '100%',
+    width: 'auto',
+    height: 'auto',
+    transform: 'translateX(-50%) translateY(-50%)',
+    backgroundColor: theme.palette.background.paper,
+    backgroundSize: 'cover',
+    zIndex: -10,
+  },
+  content: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: '100%',
   },
 })
 
 const Landing = ({ classes }) => {
   return (
     <Paper square className={classes.root}>
-      <Typography variant="h2">Hi,</Typography>
-      <Typography variant="h2">I'm Chris</Typography>
+      <video playsInline autoPlay muted loop className={classes.bgvid}>
+        <source src={bgvid} type="video/webm" />
+      </video>
+      <div className={classes.content}>
+        <div>
+          <Typography variant="h2">Hi,</Typography>
+          <Typography variant="h2">I'm Chris,</Typography>
+          <Typography variant="h2">web developer.</Typography>
+        </div>
+      </div>
     </Paper>
   )
 }
