8d81c197d5ea8261594b9c05fe717d8d224d8b00
chris023 <chris.meyer023@gmail.com>
2019-05-08 10:21:26 -0600

    Change primary theme color

--- src/util/material-ui/theme.js
+++ src/util/material-ui/theme.js
@@ -1,8 +1,15 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/'
 
 const Theme = createMuiTheme({
   typography: {
     useNextVariants: true,
+  },
+  palette: {
+    primary: {
+      light: '#252627',
+      main: '#181818',
+      dark: '#070707',
+    },
   },
 })
 
