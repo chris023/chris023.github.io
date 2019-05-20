ab5880272b717476c038cc213ae5da3a3467fba8
chris023 <chris.meyer023@gmail.com>
2019-05-20 14:16:30 -0600

    Add projects file

---
+++ src/util/projects/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import LocalMoviesIcon from '@material-ui/icons/LocalMovies'
+
+export const swapibox = {
+  name: 'SWAPI Box',
+  liveUrl: 'https://chris023.github.io/swapibox',
+  gitUrl: '',
+  icon: (
+    <img
+      style={{
+        width: 24,
+      }}
+      alt="SW"
+      src="https://raw.githubusercontent.com/chris023/swapibox/master/src/assets/readme/logo.png"
+    />
+  ),
+}
+
+export const movietracker = {
+  name: 'Movie Tracker',
+  liveUrl: 'https://chris023.github.io/movie-tracker-frontend',
+  gitUrl: '',
+  icon: <LocalMoviesIcon />,
+}
