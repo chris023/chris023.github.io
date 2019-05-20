404da861b5d23c9f163f2810f22aa5bec32af2e0
chris023 <chris.meyer023@gmail.com>
2019-05-20 13:10:10 -0600

    Configure deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core'
 
 import theme from './util/material-ui/theme'
-import history from './util/router/history'
 import { store, persistor } from './util/redux'
 import App from './App'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -15,11 +14,11 @@ import './index.css'
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Router history={history}>
+      <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
         <MuiThemeProvider theme={theme}>
           <App />
         </MuiThemeProvider>
-      </Router>
+      </BrowserRouter>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
